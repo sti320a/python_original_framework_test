464e2e04aca09aacf224000aea6b44f2b29c86be
sti320a <sti320a@gmail.com>
2018-12-10 23:34:25 +0900

    Created request object

---
+++ Use.py
@@ -0,0 +1,35 @@
+from app import App
+from wsgiref.simple_server import make_server
+
+
+app = App()
+
+
+@app.route('^/$', 'GET')
+def hello(request, start_response):
+    return ['Hello World']
+
+
+@app.route('^/user/$', 'POST')
+def create_user(request, start_response):
+    start_response('201 Created', [('Content-type', 'text/plain; charset=utf-8')])
+    return [b'User Created']
+
+
+@app.route('^/user/(?P<name>\w+)/$', 'GET')
+def user_detail(request, start_response, name):
+    start_response('200 OK', [('Content-type', 'text/plain; charset=utf-8')])
+    body = 'Hello {name}'.format(name=name)
+    return [body.encode('utf-8')]
+
+
+@app.route('^/user/(?P<name>\w+)/follow/$', 'POST')
+def create_user(request, start_response, name):
+    start_response('201 Created', [('Content-type', 'text/plain; charset=utf-8')])
+    body = 'Followed {name}'.format(name=name)
+    return [body.encode('utf-8')]
+
+
+if __name__ == '__main__':
+    httpd = make_server('', 5000, app)
+    httpd.serve_forever()
--- app.py
+++ app.py
@@ -7,6 +7,25 @@ def http405(env, start_response):
     start_response('405 Method Not Allowed', [('Content-type', 'text/plain; charset=utf-8')])
     return ['405 Method Not Allowed']
 
+class Request():
+    def __init__(self,environ):
+        self.environ = environ
+        self._body = None
+
+    @property
+    def path(self):
+        return self.environ['PATH_INFO'] or '/'
+    
+    @property
+    def method(self):
+        return self.environ['REQUEST_METHOD'].upper()
+
+    @property
+    def body(self):
+        if self._body is None:
+            content_length = int(self.environ.get('CONTENT_LENGTH', 0))
+            self._body = self.environ['wsgi.input'].read(content_length)
+            return self._body
 
 class Router():
     def __init__(self):
@@ -20,17 +39,21 @@ class Router():
         })
 
     def match(self, method, path):
+        error_callback = http404
         # pathがroutesに登録されているかを調べる
         for route in self.routes:
             matched = re.compile(route['path']).match(path)
-            if matched and route['method'] == method:
-                url_vars = matched.groupdict()
+            if not matched:
+                continue
+            
+            error_callback = http405
+            url_vars = matched.groupdict()
+            if route['method'] == method:
                 return route['callback'], url_vars
-        return http404, {}
+        return error_callback, {}
 
 
 class App():
-
     def __init__(self):
         self.router = Router()
 
@@ -42,11 +65,6 @@ class App():
 
     # Appをcallable(呼び出し可能)に
     def __call__(self, env, start_response):
-        # リクエストヘッダーからメソッド POST/GETなどを受け取る
-        method = env['REQUEST_METHOD'].upper()
-        # リクエストヘッダーからパスの情報を受け取る
-        path = env['PATH_INFO'] or '/'
-        callback, kwargs = self.router.match(method, path)
-        return callback(env, start_response, **kwargs)
-
-    
+        request = Request(env)
+        callback, kwargs = self.router.match(request.method, request.path)
+        return callback(request, start_response, **kwargs)
